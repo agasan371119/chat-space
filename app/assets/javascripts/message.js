08d11468fc8ac48456c47bac41831b27d0405efe
agasan371119 <yna77351unichi@icloud.com>
2020-03-06 08:37:18 +0900

    add

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -20,6 +20,7 @@ $(function() {
   return html;
   }
   $('#new_message').on('submit', function(e) {
+    console.log("test")
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
